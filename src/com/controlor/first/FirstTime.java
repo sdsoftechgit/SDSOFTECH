cf96d76dd0d81f416970188d2dd4688cb2865d5c
Sruthin <sruthin.reddy@gmail.com>
2016-04-01 18:04:23 +0530

    added the java class

---
+++ src/com/controlor/first/FirstTime.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+/**
+ * @author LAP-40
+ *
+ */
+package com.controlor.first;
+
+public class FirstTime{
+	
+	
+	public FirstTime() {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hi How r u ");
+	}
+	
+	
+}
